72ce1131a51c487bb503c8fcdee4aff40200a9fa
Zach Silveira <zach@encryption.io>
2018-04-18 10:31:36 -0400

    Update init.php

--- plugin/src/init.php
+++ plugin/src/init.php
@@ -1,33 +1,16 @@
 <?php
-
-// Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
-/**
- * Blocks Initializer
- *
- * Enqueue JS of all the blocks.
- *
- * @since 	1.0.0
- * @package CGB
- */
-
-// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 function crossfield_blocks_assets() {
 	wp_enqueue_script(
-		'crossfield_blocks-cgb-block-js', // Handle.
+		'crossfield_blocksjs',
 
 		//IN PRODUCTION put plugins_url( '/build/main.js', dirname( __FILE__ ) )
-		'http://localhost:8080/main.js', // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element' ) // Dependencies, defined above.
+		'http://localhost:8080/main.js', 
+		array( 'wp-blocks', 'wp-i18n', 'wp-element' ) 
 	);
 } 
 
-// Hook: Editor assets.
 add_action( 'enqueue_block_editor_assets', 'crossfield_blocks_assets' );
