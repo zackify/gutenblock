444b762acbe9efc01248af829bfeff5c74d6378a
Zach Silveira <zackify@gmail.com>
2018-04-27 13:34:54 -0400

    Inject webpack for access in your own projects when using custom config

--- config/load-custom-config.js
+++ config/load-custom-config.js
@@ -1,9 +1,10 @@
 //Load custom babel and webpack config
+const webpack = require('webpack');
 
 module.exports = () => {
   let customConfig = {};
   try {
-    customConfig = require(process.cwd() + '/gutenblock.config');
+    customConfig = require(process.cwd() + '/gutenblock.config')(webpack);
     console.log('Using custom configuration');
   } catch (e) {
     console.log('Using default configuration');
