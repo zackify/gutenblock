28ff3b2007175e4ea4305687ffc650abe68b0771
Zach Silveira <zackify@gmail.com>
2018-05-07 20:59:50 -0400

    Update example editor block

--- plugin/src/notes/edit.js
+++ plugin/src/notes/edit.js
@@ -1,19 +1,13 @@
-
-import { Input, Repeat } from 'gutenblock-controls';
+import { Input, Repeat, Editor } from 'gutenblock-controls';
 
 const Edit = ({ attributes, setAttributes }) => {
   return (
     <div>
-      <Repeat
-        attributes={attributes}
-        onChange={(name, value) => setAttributes({ [name]: value })}
-        setAttributes={setAttributes}
-        title="Notes"
-        max="50"
-        addNew="Add Item" attribute="notes"
-      >
-        <Input name="title" />
-      </Repeat>
+      <Editor>
+        <Repeat title="Notes" max="50" addNew="Add Item" attribute="notes">
+          <Input name="title" />
+        </Repeat>
+      </Editor>
     </div>
-  )
+  );
 };
