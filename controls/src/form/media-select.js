f5b487392d6fc4602117cff70759875bac89adb8
Zach <zackify@gmail.com>
2018-08-03 10:41:08 -0700

    Update to pass props to media select

--- controls/src/form/media-select.js
+++ controls/src/form/media-select.js
@@ -1,28 +1,21 @@
 const { Button } = wp.components;
 const { MediaUpload } = wp.editor;
 
-export default ({ name, attributes, onChange, beforeChange, label }) => (
+export default ({ name, attributes, onChange, beforeChange, label, ...props }) => (
   <div style={{ display: 'flex' }}>
     {attributes[name] ? (
-      <img
-        src={attributes[name]}
-        style={{ height: 10, marginRight: 5, alignSelf: 'center' }}
-      />
+      <img src={attributes[name]} style={{ height: 10, marginRight: 5, alignSelf: 'center' }} />
     ) : null}
     <MediaUpload
       type="image"
+      {...props}
       value={attributes[name]}
       render={({ open }) => (
         <Button isPrimary onClick={open}>
           {label || 'Open Media Library'}
         </Button>
       )}
-      onSelect={media =>
-        onChange(
-          name,
-          beforeChange ? beforeChange(media) : media.sizes.full.url,
-        )
-      }
+      onSelect={media => onChange(name, beforeChange ? beforeChange(media) : media.sizes.full.url)}
     />
   </div>
 );
